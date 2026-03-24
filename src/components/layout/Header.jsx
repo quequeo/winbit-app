ebe0657578f0edb7a11556f9308b8d68a494ae9a
Jaime García Méndez <jaimegarciamendez@gmail.com>
2026-03-24 13:01:39 -0500

    Increase header logo to 48px mobile / 56px desktop

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -35,7 +35,7 @@ export const Header = () => {
             <img
               src="/logo-winbit.png"
               alt="Winbit"
-              className="h-10 md:h-11"
+              className="h-12 md:h-14"
               style={{ filter: 'brightness(1.15)' }}
             />
           </Link>
